77e6f779a44828598e1a488eef9fafd53b348ba7
Camilo Sánchez <info@caxtor.co>
2025-03-29 17:15:06 -0500

    upload

--- lib/universal_track_manager/controllers/concerns/universal_track_manager_concern.rb
+++ lib/universal_track_manager/controllers/concerns/universal_track_manager_concern.rb
@@ -43,7 +43,6 @@ module UniversalTrackManagerConcern
   def new_visitor
     return nil if permitted_utm_params[:utm_source].blank?
 
-    store_id = (@store.id if @store.present?)
     params = {
       first_pageload: now,
       last_pageload: now,
@@ -62,6 +61,7 @@ module UniversalTrackManagerConcern
   end
 
   def track_visitor
+    store_id = (@store.id if @store.present?)
     if session["pze_visit_id"]
       # existing visit
       begin
@@ -95,7 +95,7 @@ module UniversalTrackManagerConcern
   def any_utm_params?
     return false unless UniversalTrackManager.track_utms?
 
-    UniversalTrackManager.campaign_column_symbols.any? do |key|
+    UniversalTrackManager.campaign_column_hashed.any? do |key|
       params[key].present?
     end
   end
@@ -118,8 +118,6 @@ module UniversalTrackManagerConcern
 
     gen_sha1 = gen_campaign_key(hashed_utm_params)
 
-    store_id = (@store.id if @store.present?)
-
     request_campaign = request.url.split("?")[0]
 
     gclid_present = UniversalTrackManager.track_gclid_present? && permitted_utm_params[:gclid].present?
@@ -140,7 +138,6 @@ module UniversalTrackManagerConcern
   end
 
   def evict_visit!(old_visit)
-    store_id = (@store.id if @store.present?)
     @visit_evicted = true
     params = {
       first_pageload: now,
--- lib/version.rb
+++ lib/version.rb
@@ -1,3 +1,3 @@
 module UniversalTrackManager
-  VERSION = "0.8.23"
+  VERSION = "0.8.24"
 end
